5c2aa77a04fac6a7b7bd645af795ed95bf5b648f
Yesseli <yesselimateo@gmail.com>
2019-06-01 08:06:37 -0600

    Examen Final

---
+++ ExamenFinal.py
@@ -0,0 +1,19 @@
+#Lógica de sistemas.
+#Primer semestre
+#Yessenia Elizabeth Mateo López.
+#0907-19-16095
+from tkinter import *    # Carga módulo tk (widgets estándar)
+from tkinter import ttk  # Carga ttk (para widgets nuevos 8.5+)
+#importe la libreria
+#....................................................................
+raiz = Tk()
+raiz.geometry('300x200') # anchura x altura
+raiz.configure(bg = 'beige')
+raiz.title('Examen final')
+ttk.Button(raiz, text='Salir', command=quit).pack(side=BOTTOM)
+raiz.mainloop()
+#Hice la interfaz gráfica
+#....................................................................
+
+        
+    
